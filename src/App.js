982de577e7a9a35d1e5b67444e7add298333816f
Bryan Kim <bkim1080@gmail.com>
2022-08-12 14:35:16 -0400

    Fetch data from WorldTime API and pass to Clock component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
+import { useState, useEffect } from "react";
+
 import Clock from "./components/Clock";
 import InfoPanel from "./components/InfoPanel";
 import Quote from "./components/Quote";
 
 function App() {
+	const [info, setInfo] = useState({ datetime: "" });
+	const [error, setError] = useState(null);
+
+	const time = info.datetime.slice(11, 16);
+	const hour = parseInt(info.datetime.slice(11, 13));
+	const timezone = info.abbreviation;
+
+	const getTimeData = async function () {
+		try {
+			const response = await fetch("http://worldtimeapi.org/api/ip");
+			if (!response.ok) {
+				throw new Error("Problem getting data");
+			}
+			const data = await response.json();
+			setInfo(data);
+		} catch (err) {
+			setError(err.message);
+		}
+	};
+
+	useEffect(() => {
+		getTimeData();
+	}, []);
+
+	// console.log(info);
+
 	return (
 		<>
 			<Quote />
-			<Clock />
+			<Clock time={time} hour={hour} timezone={timezone} error={error} />
 			<InfoPanel />
 		</>
 	);
