34722135bc4fb76586bd78a758bfd15f365291d1
Bryan Kim <bkim1080@gmail.com>
2022-08-12 13:34:13 -0400

    Set up basic components and component structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Clock from "./components/Clock";
+import InfoPanel from "./components/InfoPanel";
+import Quote from "./components/Quote";
+
 function App() {
-	return <></>;
+	return (
+		<>
+			<Quote />
+			<Clock />
+			<InfoPanel />
+		</>
+	);
 }
 
 export default App;
