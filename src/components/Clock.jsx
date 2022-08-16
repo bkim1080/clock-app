cb7742a76e03b9b17d491078dfd45f7d16e9b6b6
Bryan Kim <bkim1080@gmail.com>
2022-08-16 19:08:57 -0400

    Refactor Clock code

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -2,19 +2,9 @@ import { useEffect, useState } from "react";
 import styles from "./Clock.module.css";
 
 export default function Clock(props) {
+	// Fetch location data
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState(null);
-	const [isNotMobile, setIsNotMobile] = useState(window.innerWidth <= 768);
-	const updateGreeting = () => {
-		setIsNotMobile(window.innerWidth <= 768);
-	};
-
-	useEffect(() => {
-		window.addEventListener("resize", updateGreeting);
-		return () => {
-			window.removeEventListener("resize", updateGreeting);
-		};
-	});
 
 	const getLocationInfo = async function () {
 		setIsLoading(true);
@@ -38,7 +28,20 @@ export default function Clock(props) {
 		// getLocationInfo();
 	}, []);
 
-	// Render Greeting Message
+	// Create state for screen width
+	const [isMobileScreen, setIsMobileScreen] = useState(window.innerWidth < 768);
+	const updateGreeting = () => {
+		setIsMobileScreen(window.innerWidth <= 768);
+	};
+
+	useEffect(() => {
+		window.addEventListener("resize", updateGreeting);
+		return () => {
+			window.removeEventListener("resize", updateGreeting);
+		};
+	}, []);
+
+	// Render greeting message
 	const selectGreetingMobile = () => {
 		if (props.hour >= 5 && props.hour < 12) {
 			return "good morning";
@@ -58,7 +61,8 @@ export default function Clock(props) {
 			return "good night, it's currently";
 		}
 	};
-	// Render Symbol
+
+	// Render symbol
 	const selectSymbol = () => {
 		if (5 <= props.hour <= 17) {
 			return <img className={styles.symbol} src="/assets/desktop/icon-sun.svg" alt="sun" />;
@@ -67,7 +71,7 @@ export default function Clock(props) {
 		}
 	};
 
-	// Render City and State
+	// Render location data
 	const [locationInfo, setLocationInfo] = useState({
 		data: {
 			location: {
@@ -80,13 +84,12 @@ export default function Clock(props) {
 			},
 		},
 	});
-
 	const cityName = locationInfo.data.location.city.name;
 	const stateName = locationInfo.data.location.region.name;
 
-	const locationContent = function () {
+	const setLocationContent = function () {
 		if (cityName !== "" && stateName !== "") {
-			return `in ${((<span>{cityName}</span>), (<span>{stateName}</span>))}`;
+			return `in ${cityName}, ${stateName}`;
 		}
 		if (error) {
 			return error;
@@ -100,7 +103,7 @@ export default function Clock(props) {
 		<section className={props.isPanelOpen ? `${styles["container-active"]}` : styles.container}>
 			<div className={`${styles[""]}`}>
 				{selectSymbol()}
-				<span className={styles.greeting}>{isNotMobile ? selectGreetingMobile() : selectGreeting()}</span>
+				<span className={styles.greeting}>{isMobileScreen ? selectGreetingMobile() : selectGreeting()}</span>
 			</div>
 			<div className={`${styles["container-time"]}`}>
 				<span className={styles.time}>{props.time}</span>
@@ -108,7 +111,7 @@ export default function Clock(props) {
 			</div>
 			{/* {Placeholder to prevent API requests} */}
 			<div className={`${styles["location-info"]}`}>in Baltimore, Maryland</div>
-			{/* <div className={`${styles["location-info"]}`}>{locationContent()}</div> */}
+			{/* <div className={`${styles["location-info"]}`}>{setLocationContent()}</div> */}
 		</section>
 	);
 }
