61639ae4fbdbfba17f44017e445c7017e6a3c4a0
Bryan Kim <bkim1080@gmail.com>
2022-08-16 16:37:36 -0400

    Add InfoPanel tablet  style changes

--- src/components/InfoPanel.jsx
+++ src/components/InfoPanel.jsx
@@ -3,24 +3,24 @@ import styles from "./InfoPanel.module.css";
 export default function InfoPanel(props) {
 	return (
 		<section className={props.isNight ? `${styles["info-panel-night"]}` : `${styles["info-panel-day"]}`}>
-			<ol className={`${styles["list"]}`}>
-				<li className={`${styles["list-item"]}`}>
+			<div className={`${styles["container-content"]}`}>
+				<div className={`${styles["container-data"]}`}>
 					<span className={`${styles["category"]}`}>CURRENT TIMEZONE</span>
 					<span className={`${styles["data"]}`}>{props.timezoneName}</span>
-				</li>
-				<li className={`${styles["list-item"]}`}>
+				</div>
+				<div className={`${styles["container-data"]}`}>
 					<span className={`${styles["category"]}`}>DAY OF THE YEAR</span>
 					<span className={`${styles["data"]}`}>{props.dayOfYear}</span>
-				</li>
-				<li className={`${styles["list-item"]}`}>
+				</div>
+				<div className={`${styles["container-data"]}`}>
 					<span className={`${styles["category"]}`}>DAY OF THE WEEK</span>
 					<span className={`${styles["data"]}`}>{props.dayOfWeek}</span>
-				</li>
-				<li className={`${styles["list-item"]}`}>
-					<span className={`${styles["category"]}`}>WEEK NUMBER</span>
+				</div>
+				<div className={`${styles["container-data"]}`}>
+					<span className={`${styles["data-category"]}`}>WEEK NUMBER</span>
 					<span className={`${styles["data"]}`}>{props.weekNum}</span>
-				</li>
-			</ol>
+				</div>
+			</div>
 		</section>
 	);
 }
